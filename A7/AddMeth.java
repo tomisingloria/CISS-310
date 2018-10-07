d45742c295c232f6c17ccf28840278a66418dd5f
tomisingloria <42788922+tomisingloria@users.noreply.github.com>
2018-10-07 13:41:11 -0400

    Create AddMeth.java

---
+++ A7/AddMeth.java
@@ -0,0 +1,31 @@
+class Vehicle {
+	int passengers;
+	int fuelcap;
+	int mpg;
+	
+	void range() {
+		System.out.println("Range is " + fuelcap * mpg);
+	}
+}
+
+class AddMeth {
+	public static void main(String args[]) {
+		Vehicle minivan = new Vehicle();
+		Vehicle sportscar = new Vehicle();
+		
+		int range1, range2;
+		
+		minivan.passengers = 7;
+		minivan.fuelcap = 16;
+		minivan.mpg = 21;
+		
+		sportscar.passengers = 2;
+		sportscar.fuelcap = 14;
+		sportscar.mpg = 12;
+		
+		System.out.print("Minivan can carry " + minivan.passengers + ". ");
+		minivan.range();
+		System.out.print("Sportscar can carry " + sportscar.passengers + ". ");
+		sportscar.range();
+	}
+}
